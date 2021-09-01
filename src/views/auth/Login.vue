962365fce6581629d490c93b0aa059396ced48da
Burak <wizard1456@hotmail.com>
2021-09-01 18:58:08 +0300

    Login edited

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,13 +1,50 @@
 <template>
-      <q-input outlined v-model="text" label="User Name" />
-      <q-input outlined v-model="text" label="Key" />
-    <q-btn color="primary" ripple label="Got it!" />
+  <div class="q-pa-md" style="max-width: 400px">
+    <div class="q-gutter-md">
+      <q-input
+        filled
+        v-model="username"
+        label="Your Username *"
+        hide-bottom-space
+        :rules="[(val) => (val && val.length > 0) || 'Please enter username!']"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="key"
+        label="Your Key *"
+        hide-bottom-space
+        counter
+        :rules="[
+          (val) => (val !== null && val !== '') || 'Please type your key',
+          (val) => (val.length > 50 && val.length < 100) || 'Please type a real key',
+        ]"
+      />
+
+      <div>
+        <q-btn @click="auth()" label="Submit" color="primary" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      username: null,
+      key: null
+    }
+  },
+  methods: {
+    auth(){
+      if (this.username && this.key) {
+      }
+    }
+  },
+};
 </script>
 
 <style lang="">
-
 </style>
