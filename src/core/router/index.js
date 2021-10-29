43858e62561c277891a8fd7e5be81ac1189e0e87
Burak <wizard1456@hotmail.com>
2021-10-29 11:55:43 +0300

    Router auth functions created.

--- src/core/router/index.js
+++ src/core/router/index.js
@@ -1,5 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router"
 
+const isAuth = (to, from, next) => {
+    next();
+}
+
+const isAccess = (role, to, from, next) => {
+    next();
+}
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -11,11 +19,17 @@ const router = createRouter({
         {
             path: '/',
             redirect: { name: 'Bash' },
+            beforeEnter: (to, from, next) => {
+                isAuth(to, from, next);
+            },
             component: () => import("@/layouts/Container.vue"),
             children: [
                 {
                     name: 'Bash',
                     path: '/bash',
+                    beforeEnter: (to, from, next) => {
+                        isAccess(store.currentUser.role, to, from, next)
+                    },
                     component: () => import('@/views/bash/Bash.vue')
                 }
             ]
